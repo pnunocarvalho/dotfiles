eeca8d674075b672a1e2b015c0601921c751a97c
Pedro Carvalho <incude@gmail.com>
2015-12-15 21:48:55 +0000

    So long fish, still love your auto completion.
    Why y no POSIX?????

--- config/fish/config.sh
+++
@@ -1,2 +0,0 @@
-set -x GOPATH $HOME/code/go
-set -x PATH /usr/local/bin /usr/local/sbin $HOME/scripts $HOME/bin /usr/local/heroku/bin $GOPATH/bin $HOME/.rbenv/bin $NODE_PATH:/usr/local/lib/node_modules
